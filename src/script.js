f1c5e2206e8f12ff5bb101ce14dd494087d977eb
Gugun1908 <gugun92.ll@gmail.com>
2024-11-03 02:52:33 +0700

    Mengatur responsive

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
+// //toogle icon nav
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
 // scroll section active link
 
 let sections = document.querySelectorAll("section");
@@ -17,7 +26,26 @@ window.onscroll = () => {
       });
     }
   });
+
+  //sticky navbar
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove toggle icon and navbar when click navbar link (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
-//toogle icon nav
-let menuIcon = document.querySelector;
+// scroll reveal
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-img,.services-container, .portofolio-box, .contact form", { origin: "bottom" });
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
